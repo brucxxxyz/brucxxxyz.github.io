f738e5eefcb6a108c4f207e89011fd70098c6e4b
brucxxxyz <brucxxxyz@gmail.com>
2025-12-30 06:13:33 +1300

    Create lang.js

---
+++ data/lang.js
@@ -0,0 +1,23 @@
+{
+  "zh-CN": {
+    "nav_home": "🏠 首页",
+    "nav_breakfast": "🍳 早餐",
+    "nav_play": "🗺 游玩",
+    "nav_fitness": "💪 健身",
+    "footer": "© 我的生活助手"
+  },
+  "zh-HK": {
+    "nav_home": "🏠 主頁",
+    "nav_breakfast": "🍳 早餐",
+    "nav_play": "🗺 玩樂",
+    "nav_fitness": "💪 健身",
+    "footer": "© 我的生活助手"
+  },
+  "en": {
+    "nav_home": "🏠 Home",
+    "nav_breakfast": "🍳 Breakfast",
+    "nav_play": "🗺 Activities",
+    "nav_fitness": "💪 Fitness",
+    "footer": "© My Daily Assistant"
+  }
+}
